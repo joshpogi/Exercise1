316ae254962de8ecc6b2c0406f99bc110c998c02
joshpogi <joshua.tariga@gmail.com>
2014-08-29 16:17:59 +0800

    Completed exercise

---
+++ jeje1.java
@@ -0,0 +1,14 @@
+
+public class jeje{
+	public static void main(String []args){
+	int x, ctr=0;
+	for( x=1;x<=100;x++){
+		
+		ctr+=x;
+		System.out.println(ctr);
+	}
+		
+		System.out.println("joshua_0395@pogi.com");
+		System.out.println("Secret Code");
+	}
+}
