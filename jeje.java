d13a720f895d5d080cd6285762d668e2ce8c2dd8
joshpogi <joshua.tariga@gmail.com>
2014-08-29 16:15:38 +0800

    wee

---
+++ jeje.java
@@ -0,0 +1,12 @@
+
+public class jeje{
+	public static void main(String []args){
+	int x,ctr;
+	for( x=1;x<=100;x++){
+		ctr+=x;
+	}
+		System.out.println(x);
+		System.out.println("joshua_0395@pogi.com");
+		System.out.println("Secret Code");
+	}
+}
