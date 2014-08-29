2ea1381950f32745207f76f0635ab0fa74784692
Andre' Ruiz Alvaro <andre39alvaro@yahoo.com>
2014-08-29 16:41:25 +0800

    index2

---
+++ index2.java
@@ -0,0 +1,14 @@
+public class index2 {
+				
+	public static void main (String[]args){
+		int year = 1994;
+		String color = "blue";
+		
+		System.out.println("MY SECRET CODE IS " + year + " + " + color + ".");
+		System.out.println("First line ouput from index.java.");
+		
+	}
+	
+	
+	
+}
